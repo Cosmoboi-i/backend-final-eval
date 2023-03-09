2be5cc0d881054feb6fa3facaccabdc26e071e29
Aganya Bajaj <aganyabajaj7@gmail.com>
2023-03-09 16:42:38 +0530

    feat: create createCollectionEntry and UpdateCollectionEntry APIs

--- src/controllers/collection.controller.js
+++ src/controllers/collection.controller.js
@@ -22,9 +22,34 @@ const getCollectionEntryById = async (req, res, next) => {
   }
 };
 
+const createCollectionEntry = async (req, res, next) => {
+  try {
+    const { content, content_type_id } = req.body;
+    const collection = await collectionService.createCollectionEntry(content, content_type_id);
+    res.status(201).json({ message: "entry created", collection, success: true });
+  }
+  catch (e) {
+    next(e);
+  }
+}
 
+const updateCollectionEntry = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { content, content_type_id } = req.body;
+    const collection = await collectionService.updateCollectionEntry(id, content, content_type_id);
+    res.status(200).json({ message: "entry updated", collection, success: true });
+  }
+  catch (e) {
+    next(e);
+  }
+}
+
+}
 
 module.exports = {
   getCollectionByContentType,
   getCollectionEntryById,
+  createCollectionEntry,
+  updateCollectionEntry,
 }
--- src/services/collection.service.js
+++ src/services/collection.service.js
@@ -16,8 +16,23 @@ const getCollectionEntryById = async (id) => {
   return entry;
 }
 
+const createCollectionEntry = async (content, content_type_id) => {
+  console.log(content, content_type_id);
+  const entry = await collection.create({ content, content_type_id });
+  console.log(entry);
+  if (!entry) throw new Error('Entry not created');
+  return entry;
+}
+
+const updateCollectionEntry = async (id, content, content_type_id) => {
+  const entry = await collection.update({ content, content_type_id }, { where: { id } });
+  return entry;
+}
+
 
 module.exports = {
   getCollectionsByContentType,
   getCollectionEntryById,
+  createCollectionEntry,
+  updateCollectionEntry,
 }
